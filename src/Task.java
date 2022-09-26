192d80473bd5fbb39c1cf5f2b750825573d60b3b
Igor <99yearago99@gmail.com>
2022-09-26 20:52:07 +0300

    Task 2

--- src/Task.java
+++ src/Task.java
@@ -1,5 +1,20 @@
 public class Task {
     public static void main(String[] args) {
-        System.out.println("Task_4.3");
+
+        int ageChild = 6;
+
+        boolean canNot= ageChild<5;
+        boolean onlyWithAdult = ageChild >=5 && ageChild <14;
+        boolean alone = ageChild >=14;
+
+        if (canNot){
+            System.out.println("Если ребенку " + ageChild +" лет, ему нельзя кататься на аттракционе");
+        }
+        else if (onlyWithAdult){
+            System.out.println("Если ребенку " + ageChild + " лет, ему можно кататься на аттракционе только со взрослым");
+        }
+        else if (alone){
+            System.out.println("Если ребенку "+ ageChild +" лет, ему можно кататься на аттракционе одному");
+        }
     }
 }
