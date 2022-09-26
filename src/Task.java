8acfcb00077ffc9408e601b6c47530f5776698db
Igor <99yearago99@gmail.com>
2022-09-26 21:24:52 +0300

    Task 3

--- src/Task.java
+++ src/Task.java
@@ -1,5 +1,41 @@
 public class Task {
     public static void main(String[] args) {
-        System.out.println("Task_4.3");
+
+        int one = 4;
+        int two = 2;
+        int three = 3;
+
+        boolean oneBigger = one>two && one>three;
+        boolean twoBigger = two>one && two>three;
+        boolean threeBigger = three>one && three>two;
+        boolean oneSameTwo = one == two;
+        boolean oneSameThree = one == three;
+        boolean twoSameThree = two == three;
+
+        if (oneBigger){
+            System.out.println(one + " наибольшее число");
+        }
+        else if (twoBigger){
+            System.out.println(two + " наибольшее число");
+        }
+        else if (threeBigger){
+            System.out.println(three + " наибольшее число");
+        }
+
+        else if(one==two && two==three){
+            System.out.println("Числа равны");
+        }
+
+        else if(oneSameTwo){
+            System.out.println("Числа " + one + " и " + two + " равны");
+        }
+        else if(oneSameThree){
+            System.out.println("Числа " + one + " и " + three + " равны");
+        }
+        else if(twoSameThree){
+            System.out.println("Числа " + three + " и " + two + " равны");
+        }
+
+
     }
 }
